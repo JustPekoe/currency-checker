29cd0a47742ec0f260e997cd6b53020ec07385ce
Posy Au-Yeung <posyay@hotmail.com>
2024-08-19 15:03:11 -0700

    Add files via upload
    Add baritone logic
    Add TODO list of items to add to the converter

--- currency-checler/src/main/java/currency_converter/ConvertCurrency.java
+++ currency-checler/src/main/java/currency_converter/ConvertCurrency.java
@@ -10,13 +10,47 @@ public class ConvertCurrency {
         // Initialize WebDriver
         WebDriver driver = new ChromeDriver();
 
-        // Open a webpage
-        driver.get("https://www.google.com");
+        //TODO: Display available banks and credit cards that can be used in the converter
+
+        //TODO: Get user input of what banks/credit cards to to get currency of
+
+        // Open banks webpages (RBC, CIBC, TD, etc.)
+        ConvertCurrency bankCurrencyValidator = new ConvertCurrency();
+        bankCurrencyValidator.openBankWebpages(driver);
+
+
+        // Open credit card webpages (E.g. Mastercard, Visa, etc.)
+        ConvertCurrency creditCardCurrencyValidator = new ConvertCurrency();
+        creditCardCurrencyValidator.openBankWebpages(driver);
 
         // Print the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
         // Close the browser
         driver.quit();
+    }
+
+    private void openBankWebpages(WebDriver driver){
+        String[] listOfBanks = {"CIBC", "RBC", "TD", "Tangerine"};
+
+        for (String bank : listOfBanks) {
+            if (bank.equals("CIBC")) {
+                driver.get("https://www.google.com");
+            } else if (bank.equals("RBC")) {
+                driver.get("https://www.google.com");
+            } else if (bank.equals("Tangerine")) {
+                driver.get("https://www.google.com");
+            }else if (bank.equals("TD")) {
+                driver.get("https://www.google.com");
+            } else {
+                System.out.println("Bank name is not valid. " +
+                                "Please enter one of these banks: ");
+            }
+        }
+    }
+
+    private void openCreditCardWebpages() {
+        String[] listOfCreditCards = {"Mastercard", "Visa"};
+
     }
 }
