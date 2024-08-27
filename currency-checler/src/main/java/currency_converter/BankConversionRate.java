5469e69c3f7504f2db6db38cb02b14d8e1fdc405
Posy Au-Yeung <posyay@hotmail.com>
2024-08-27 08:07:14 -0700

    Add files via upload

--- currency-checler/src/main/java/currency_converter/BankConversionRate.java
+++ currency-checler/src/main/java/currency_converter/BankConversionRate.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class BankConversionRate {
-    private WebDriver driver;
+    private final WebDriver driver;
 
     // Banks URL
     private static final String rbcUrl =
@@ -20,6 +20,9 @@ public class BankConversionRate {
     }
 
     protected String rbcConversionRate(int cadAmount, String currency) {
+        // Go to RBC's CAD to USD conversion calculator
+        driver.get(rbcUrl);
+
         System.out.println(STR."Converting $\{cadAmount}CAD to \{currency}");
         String convertedAmount = null;
 
@@ -35,12 +38,28 @@ public class BankConversionRate {
 
         convertedAmount = currencyWantAmount.getAttribute("value");
 
-
         return convertedAmount;
     }
 
     protected String tdConversionRate(int cadAmount, String currency) {
-        return null;
-    }
+        // Go to TD's CAD to USD conversion calculator
+        driver.get(tdUrl);
 
+        System.out.println(STR."Converting $\{cadAmount}CAD to \{currency}");
+        String convertedAmount = null;
+
+        // Add input to the "Current I Have"
+        WebElement currencyHaveAmount = driver.findElement(By.id("currency-have-amount"));
+        currencyHaveAmount.sendKeys(String.valueOf(cadAmount));
+
+        // Pressing enter without a specific target
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+
+        // Get the currency that I want after conversion is completed
+        WebElement currencyWantAmount = driver.findElement(By.id("currency-want-amount"));
+
+        convertedAmount = currencyWantAmount.getAttribute("value");
+        
+        return convertedAmount;
+    }
 }
